5faf1abc4e2021f36164408e881e3d7309cc77d6
Chris La Rose <cjlarose@iplantcollaborative.org>
2012-01-13 12:59:28 -0700

    Basic proof-of-concept, complete with url routing

--- index.php
+++ index.php
@@ -6,10 +6,39 @@ require_once BASE_DIR . 'utils.php';
 // make an iRODS account object for connection, assuming:
 // username: demouser, password: demopass, server: srbbrick15.sdsc.edu, port: 1247
 $account = new RODSAccount($config['server'], $config['port'], $config['username'], $config['password']);
-//create an file object for read, assuming the path is "/tempZone/home/demouser/test_read.txt"
-$home = new ProdsDir($account,"/" . $config['zone'] .  "/home/cjlarose");
 
-$children = $home->getAllChildren();
+#pr($_SERVER['QUERY_STRING']);
+$query = $_SERVER['QUERY_STRING'];
 
-foreach ($children as $child)
-	echo get_class($child) . " " . $child->getName() . "<br />";
+if ($query == "") {
+
+	$home = new ProdsDir($account,"/" . $config['zone'] .  "/home/cjlarose");
+
+	echo list_directory($home);
+
+} else {
+	$file = new ProdsFile($account, $query);
+	if ($file->exists()) {
+		header('Content-Description: File Transfer');
+		header('Content-Type: application/octet-stream');
+		#header('Content-Disposition: attachment; filename=' . basename($file));
+		header('Content-Transfer-Encoding: binary');
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate');
+		header('Pragma: public');
+		#header('Content-Length: ' . filesize($file));
+		ob_clean();
+		flush();
+		$file->open("r");
+		while($str=$file->read(4096))
+			echo $str;
+		//close the file pointer
+		$file->close();
+		exit;
+	} else {
+		$dir = new ProdsDir($account, $query);
+		if ($dir->exists()) {
+			echo list_directory($dir);
+		}
+	}
+}
--- utils.php
+++ utils.php
@@ -8,3 +8,64 @@ function pr() {
 		echo "</pre>";
 	}
 }
+
+function element($tag_name, $content = '', $attributes = NULL) {
+	if ($attributes) {
+	    $html_string = "<$tag_name";
+	    foreach ($attributes as $key => $value) {
+		if (in_array($key, array('selected', 'checked'))) {
+		    if ($value)
+			$html_string .= " {$key}=\"{$key}\"";
+		} else if ($value != '') {
+		    $html_string .= " {$key}=\"{$value}\"";
+		}
+	    }
+	} else {
+	    $html_string = "<$tag_name"; //$html_string = "<$tag_name>";
+	}
+
+	if (!in_array($tag_name, array('input', 'hr', 'br'))) {
+	    $html_string .= ">";
+	    $html_string .= $content;
+	    $html_string .= "</$tag_name>";
+	} else {
+	    $html_string .= " />";
+	}
+
+	return $html_string;
+}
+
+function base_url($path = '') {
+
+	return "http://buhl.iplantcollaborative.org/irods-public" . $path;
+
+}
+
+function anchor($contents, $path) {
+	return element('a', $contents, array('href' => base_url($path)));
+}
+
+function list_directory($dir) {
+	$children = $dir->getAllChildren();
+
+	$directory_items = array();
+
+	foreach ($children as $child) { 
+		$child_class = get_class($child);
+		if ($child_class == "ProdsFile") {
+			
+			$li_contents = anchor($child->getName(), $child->path_str);
+			$directory_items[] = element('li', $li_contents);
+
+		} elseif ($child_class == "ProdsDir") {
+			
+			$li_contents = anchor($child->getName() . "/", $child->path_str);
+			$directory_items[] = element('li', $li_contents);
+
+		}
+	}
+
+	$ul_contents = implode("\n", $directory_items);
+	echo element('ul', $ul_contents);
+	//echo get_class($child) . " " . $child->getName() . "<br />";
+}
