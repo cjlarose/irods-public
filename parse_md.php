071493bff69bb18f8748410c1a7bf2d910960ad2
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-02-18 16:40:51 -0700

    Added parse_md.php which converts irods markdown files into html

---
+++ parse_md.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'bootstrap.php';
+require_once 'php-markdown-1.0.1/markdown.php';
+
+if (!isset($_GET['file'])) 
+	exit('404');
+
+$file_path = $config['path'] . '/' . $_GET['file'];
+
+$account = new RODSAccount($config['server'], $config['port'], $config['username'], $config['password']);
+
+$file = new ProdsFile($account, $file_path);
+
+if (is_valid_directory($file)) {
+	$file_info = pathinfo($file->path_str);	
+	if ($file_info['extension'] == "md" || $file_info['extension'] == 'markdown') {
+		$file->open('r');
+
+		$pieces = array();
+		while ($pieces[] = $file->read(4096));
+
+		$contents = implode("", $pieces);
+		//pr($contents);
+
+		$html = Markdown($contents);
+		echo $html;
+	} else {
+		echo "Invalid file format";
+	}
+} else {
+	echo "404";
+}
